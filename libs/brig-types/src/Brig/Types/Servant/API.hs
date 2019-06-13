81eeb72073a9667e1511f5428d31648c7ddce19a
Matthias Fischmann <mf@zerobuzz.net>
2019-06-13 16:26:32 +0200

    ...

---
+++ libs/brig-types/src/Brig/Types/Servant/API.hs
@@ -0,0 +1,422 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+-- FUTUREWORK: move the 'ToSchema' instances to their home modules (where the data types
+-- live), and turn warning about orphans back on.
+
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+-- it's ok to not warn about unmatched patterns; 'validateEveryToJSON' will crash on them
+-- before too long.
+
+-- | Import this qualified.
+module Brig.Types.Servant.API where
+
+import Imports
+
+import "swagger2" Data.Swagger hiding (Header(..))
+  -- NB: this package depends on both types-common, swagger2, so there is no away around this name
+  -- clash other than -XPackageImports.
+
+import Brig.Types.Client.Prekey (PrekeyId, Prekey, LastPrekey)
+import Brig.Types.Provider
+import Brig.Types.Team.LegalHold
+import Control.Lens
+import Data.Aeson (toJSON)
+import Data.Aeson (Value(..))
+import Data.HashMap.Strict.InsOrd
+import Data.Id
+import Data.LegalHold
+import Data.Misc
+import Data.Proxy
+import Data.Text as Text (unlines)
+import Data.UUID (UUID, fromText)
+import Servant.API hiding (Header)
+import Servant.Swagger
+import URI.ByteString.QQ (uri)
+
+{-
+import Data.String.Conversions
+import System.Process (system)
+import Data.Aeson (encode)
+import Test.Hspec (hspec)
+import Brig.Types.Test.Arbitrary ()
+
+main :: IO ()
+main = do
+  writeFile "/tmp/x" . cs $ encode swagger
+  void $ system "cat /tmp/x | json_pp && curl -X POST -d @/tmp/x -H 'Content-Type:application/json' http://online.swagger.io/validator/debug | json_pp"
+  hspec $ validateEveryToJSON (Proxy @GalleyRoutes)
+  -- see also: https://github.com/swagger-api/validator-badge
+
+  -- alternatives:
+  -- https://github.com/navidsh/maven.swagger.validator
+  -- https://editor.swagger.io/  (this finds dangling refs.  good.)
+  -- https://apidevtools.org/swagger-parser/online/  (also finds dangling refs, but it's *very slow*)
+-}
+
+
+
+swagger :: Swagger
+swagger = toSwagger (Proxy @GalleyRoutes)
+
+
+type API = PublicAPI :<|> InternalAPI
+
+type PublicAPI
+     = "teams" :> Capture "tid" TeamId :> "legalhold" :> "settings"
+          :> ReqBody '[JSON] NewLegalHoldService
+          :> Post '[JSON] ViewLegalHoldService
+  :<|> "teams" :> Capture "tid" TeamId :> "legalhold" :> "settings"
+          :> Get '[JSON] ViewLegalHoldService
+  :<|> "teams" :> Capture "tid" TeamId :> "legalhold" :> "settings"
+          :> Verb 'DELETE 204 '[] NoContent
+
+  :<|> "teams" :> Capture "tid" TeamId :> "legalhold" :> Capture "uid" UserId
+          :> Post '[] NoContent
+  :<|> "teams" :> Capture "tid" TeamId :> "legalhold" :> Capture "uid" UserId :> "approve"
+          :> Verb 'PUT 204 '[] NoContent
+  :<|> "teams" :> Capture "tid" TeamId :> "legalhold" :> Capture "uid" UserId
+          :> Get '[JSON] UserLegalHoldStatusResponse
+  :<|> "teams" :> Capture "tid" TeamId :> "legalhold" :> Capture "uid" UserId
+          :> Verb 'DELETE 204 '[] NoContent
+
+type InternalAPI
+     = "i" :> "status"
+         :> Get '[] NoContent
+  :<|> "i" :> "status"
+         :> Head '[] NoContent
+
+  :<|> "i" :> "monitoring"
+         :> Get '[JSON]
+    get "/i/monitoring" (continue $ const $ view metrics >>= fmap json . render) $
+        accept "application" "json"
+
+
+
+    post "/i/users/:id/auto-connect" (continue autoConnect) $
+        accept "application" "json"
+        .&. capture "id"
+        .&. opt (header "Z-Connection")
+        .&. jsonRequest @UserSet
+
+    post "/i/users" (continue createUserNoVerify) $
+        accept "application" "json"
+        .&. jsonRequest @NewUser
+
+    put "/i/self/email" (continue changeSelfEmailNoSend) $
+        header "Z-User"
+        .&. jsonRequest @EmailUpdate
+
+    delete "/i/users/:id" (continue deleteUserNoVerify) $
+        capture "id"
+
+    get "/i/users/connections-status" (continue deprecatedGetConnectionsStatus) $
+        query "users"
+        .&. opt (query "filter")
+
+    post "/i/users/connections-status" (continue getConnectionsStatus) $
+        accept "application" "json"
+        .&. jsonRequest @ConnectionsStatusRequest
+        .&. opt (query "filter")
+
+    get "/i/users" (continue listActivatedAccounts) $
+        accept "application" "json"
+        .&. (param "ids" ||| param "handles")
+
+    get "/i/users" (continue listAccountsByIdentity) $
+        accept "application" "json"
+        .&. (param "email" ||| param "phone")
+
+    put "/i/users/:id/status" (continue changeAccountStatus) $
+        capture "id"
+        .&. jsonRequest @AccountStatusUpdate
+
+    get "/i/users/:id/status" (continue getAccountStatus) $
+        accept "application" "json"
+        .&. capture "id"
+
+    get "/i/users/:id/contacts" (continue getContactList) $
+        accept "application" "json"
+        .&. capture "id"
+
+    get "/i/users/activation-code" (continue getActivationCode) $
+        accept "application" "json"
+        .&. (param "email" ||| param "phone")
+
+    get "/i/users/password-reset-code" (continue getPasswordResetCode) $
+        accept "application" "json"
+        .&. (param "email" ||| param "phone")
+
+    post "/i/users/revoke-identity" (continue revokeIdentity) $
+        param "email" ||| param "phone"
+
+    head "/i/users/blacklist" (continue checkBlacklist) $
+        param "email" ||| param "phone"
+
+    delete "/i/users/blacklist" (continue deleteFromBlacklist) $
+        param "email" ||| param "phone"
+
+    post "/i/users/blacklist" (continue addBlacklist) $
+        param "email" ||| param "phone"
+
+    -- given a phone number (or phone number prefix), see whether
+    -- it is blocked via a prefix (and if so, via which specific prefix)
+    get "/i/users/phone-prefixes/:prefix" (continue getPhonePrefixes) $
+        capture "prefix"
+
+    delete "/i/users/phone-prefixes/:prefix" (continue deleteFromPhonePrefix) $
+        capture "prefix"
+
+    post "/i/users/phone-prefixes" (continue addPhonePrefix) $
+      accept "application" "json"
+      .&. jsonRequest @ExcludedPrefix
+
+    -- is :uid not team owner, or there are other team owners?
+    get "/i/users/:uid/can-be-deleted/:tid" (continue canBeDeleted) $
+      capture "uid"
+      .&. capture "tid"
+
+    -- is :uid team owner (the only one or one of several)?
+    get "/i/users/:uid/is-team-owner/:tid" (continue isTeamOwner) $
+      capture "uid"
+      .&. capture "tid"
+
+    put "/i/users/:uid/sso-id" (continue updateSSOId) $
+      capture "uid"
+      .&. accept "application" "json"
+      .&. jsonRequest @UserSSOId
+
+    put "/i/users/:uid/managed-by" (continue updateManagedBy) $
+      capture "uid"
+      .&. accept "application" "json"
+      .&. jsonRequest @ManagedByUpdate
+
+    put "/i/users/:uid/rich-info" (continue updateRichInfo) $
+      capture "uid"
+      .&. accept "application" "json"
+      .&. jsonRequest @RichInfoUpdate
+
+    post "/i/clients" (continue internalListClients) $
+      accept "application" "json"
+      .&. jsonRequest @UserSet
+
+
+
+
+
+
+
+
+
+
+
+
+
+instance ToParamSchema (Id a) where
+    toParamSchema _ = toParamSchema (Proxy @UUID)
+
+instance ToSchema (Id a) where
+    declareNamedSchema _ = declareNamedSchema (Proxy @UUID)
+
+instance ToSchema HttpsUrl where
+    declareNamedSchema _ = declareNamedSchema (Proxy @Text)
+
+instance ToSchema ServiceKeyPEM where
+    declareNamedSchema _ = tweak $ declareNamedSchema (Proxy @Text)
+      where
+        tweak = fmap $ schema . example ?~ pem
+        pem = String . Text.unlines $
+            [ "-----BEGIN PUBLIC KEY-----"
+            , "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAu+Kg/PHHU3atXrUbKnw0"
+            , "G06FliXcNt3lMwl2os5twEDcPPFw/feGiAKymxp+7JqZDrseS5D9THGrW+OQRIPH"
+            , "WvUBdiLfGrZqJO223DB6D8K2Su/odmnjZJ2z23rhXoEArTplu+Dg9K+c2LVeXTKV"
+            , "VPOaOzgtAB21XKRiQ4ermqgi3/njr03rXyq/qNkuNd6tNcg+HAfGxfGvvCSYBfiS"
+            , "bUKr/BeArYRcjzr/h5m1In6fG/if9GEI6m8dxHT9JbY53wiksowy6ajCuqskIFg8"
+            , "7X883H+LA/d6X5CTiPv1VMxXdBUiGPuC9IT/6CNQ1/LFt0P37ax58+LGYlaFo7la"
+            , "nQIDAQAB"
+            , "-----END PUBLIC KEY-----"
+            ]
+
+instance ToSchema (Fingerprint Rsa) where
+    declareNamedSchema _ = declareNamedSchema (Proxy @Text)  -- TODO (at least inject a plausible example)
+
+instance ToSchema ServiceToken where
+    declareNamedSchema _ = declareNamedSchema (Proxy @Text)  -- TODO (at least inject a plausible example)
+
+instance ToSchema NewLegalHoldService where
+    declareNamedSchema = genericDeclareNamedSchema opts
+      where
+        opts = defaultSchemaOptions
+          { fieldLabelModifier = \case
+              "newLegalHoldServiceKey"   -> "public_key"
+              "newLegalHoldServiceUrl"   -> "base_url"
+              "newLegalHoldServiceToken" -> "auth_token"
+          }
+
+instance ToSchema ViewLegalHoldService where
+    declareNamedSchema _ = pure $ NamedSchema (Just "ViewLegalHoldService") $ mempty
+        & properties .~ properties_
+        & example .~ example_
+        & required .~ ["status"]
+        & minProperties .~ Just 1
+        & maxProperties .~ Just 2
+        & type_ .~ SwaggerObject
+      where
+        properties_ :: InsOrdHashMap Text (Referenced Schema)
+        properties_ = fromList
+          [ ("status", Inline (toSchema (Proxy @MockViewLegalHoldServiceStatus)))
+          , ("info", Inline (toSchema (Proxy @ViewLegalHoldServiceInfo)))
+          ]
+
+        example_ :: Maybe Value
+        example_ = Just . toJSON
+                 $ ViewLegalHoldService (ViewLegalHoldServiceInfo (Id tid) lhuri fpr)
+          where
+            Just tid = fromText "7fff70c6-7b9c-11e9-9fbd-f3cc32e6bbec"
+            Right lhuri = mkHttpsUrl [uri|https://example.com/|]
+            fpr = Fingerprint "\138\140\183\EM\226#\129\EOTl\161\183\246\DLE\161\142\220\239&\171\241h|\\GF\172\180O\129\DC1!\159"
+
+-- | this type is only introduce locally here to generate the schema for 'ViewLegalHoldService'.
+data MockViewLegalHoldServiceStatus = Configured | NotConfigured | Disabled
+  deriving (Eq, Show, Generic)
+
+instance ToSchema MockViewLegalHoldServiceStatus where
+    declareNamedSchema = genericDeclareNamedSchema opts
+      where
+        opts = defaultSchemaOptions { constructorTagModifier = camelToUnderscore }
+
+instance ToSchema ViewLegalHoldServiceInfo where
+    {-
+
+    -- FUTUREWORK: The generic instance uses a reference to the UUID type in TeamId.  This
+    -- leads to perfectly valid swagger output, but 'validateEveryToJSON' chokes on it
+    -- (unknown schema "UUID").  In order to be able to run those tests, we construct the
+    -- 'ToSchema' instance manually.
+    -- See also: https://github.com/haskell-servant/servant-swagger/pull/104
+
+    declareNamedSchema = genericDeclareNamedSchema opts
+      where
+        opts = defaultSchemaOptions
+          { fieldLabelModifier = \case
+              "viewLegalHoldServiceFingerprint" -> "fingerprint"
+              "viewLegalHoldServiceUrl"         -> "base_url"
+              "viewLegalHoldServiceTeam"        -> "team_id"
+          }
+    -}
+    declareNamedSchema _ = pure $ NamedSchema (Just "ViewLegalHoldServiceInfo") $ mempty
+        & properties .~ properties_
+        & example .~ example_
+        & required .~ ["team_id", "base_url", "fingerprint"]
+        & type_ .~ SwaggerObject
+      where
+        properties_ :: InsOrdHashMap Text (Referenced Schema)
+        properties_ = fromList
+          [ ("team_id", Inline (toSchema (Proxy @UUID)))
+          , ("base_url", Inline (toSchema (Proxy @HttpsUrl)))
+          , ("fingerprint", Inline (toSchema (Proxy @(Fingerprint Rsa))))
+          ]
+
+        example_ :: Maybe Value
+        example_ = Just . toJSON
+                 $ ViewLegalHoldServiceInfo (Id tid) lhuri fpr
+          where
+            Just tid = fromText "7fff70c6-7b9c-11e9-9fbd-f3cc32e6bbec"
+            Right lhuri = mkHttpsUrl [uri|https://example.com/|]
+            fpr = Fingerprint "\138\140\183\EM\226#\129\EOTl\161\183\246\DLE\161\142\220\239&\171\241h|\\GF\172\180O\129\DC1!\159"
+
+instance ToSchema LegalHoldTeamConfig where
+    declareNamedSchema = genericDeclareNamedSchema opts
+      where
+        opts = defaultSchemaOptions
+          { fieldLabelModifier = \case
+              "legalHoldTeamConfigStatus" -> "status"
+          }
+
+instance ToSchema LegalHoldStatus where
+    declareNamedSchema = tweak . genericDeclareNamedSchema opts
+      where
+        opts = defaultSchemaOptions
+          { constructorTagModifier = \case
+              "LegalHoldDisabled" -> "disabled"
+              "LegalHoldEnabled"  -> "enabled"
+          }
+
+        tweak = fmap $ schema . description ?~ descr
+          where
+            descr = "determines whether admins of a team " <>
+                    "are allowed to enable LH for their users."
+
+instance ToSchema RequestNewLegalHoldClient where
+    declareNamedSchema = genericDeclareNamedSchema opts
+      where
+        opts = defaultSchemaOptions
+          { fieldLabelModifier = \case
+              "userId" -> "user_id"
+              "teamId" -> "team_id"
+          }
+
+instance ToSchema NewLegalHoldClient where
+    declareNamedSchema = genericDeclareNamedSchema opts
+      where
+        opts = defaultSchemaOptions
+          { fieldLabelModifier = \case
+              "newLegalHoldClientPrekeys"     -> "prekeys"
+              "newLegalHoldClientLastKey"     -> "last_prekey"
+          }
+
+instance ToSchema UserLegalHoldStatusResponse where
+    declareNamedSchema = genericDeclareNamedSchema opts
+      where
+        opts = defaultSchemaOptions
+          { fieldLabelModifier = \case
+              "ulhsrStatus" -> "status"
+              "ulhsrLastPrekey" -> "last_prekey"
+              "ulhsrClientId" -> "client_id"
+          }
+
+instance ToSchema UserLegalHoldStatus where
+    declareNamedSchema = tweak . genericDeclareNamedSchema opts
+      where
+        opts = defaultSchemaOptions
+          { constructorTagModifier = \case
+              "UserLegalHoldEnabled"  -> "enabled"
+              "UserLegalHoldPending"  -> "pending"
+              "UserLegalHoldDisabled" -> "disabled"
+          }
+
+        tweak = fmap $ schema . description ?~ descr
+          where
+            descr = "states whether a user is under legal hold, " <>
+                    "or whether legal hold is pending approval."
+
+instance ToSchema ClientId where
+    declareNamedSchema _ = tweak $ declareNamedSchema (Proxy @Text)
+      where
+        tweak = fmap $ schema . description ?~ descr
+          where
+            descr = "A Client Id"
+
+instance ToSchema PrekeyId where
+    declareNamedSchema _ = tweak $ declareNamedSchema (Proxy @Int)
+      where
+        tweak = fmap $ schema . description ?~ descr
+          where
+            descr = "in the range [0..65535]."
+
+instance ToSchema Prekey where
+    declareNamedSchema = genericDeclareNamedSchema opts
+      where
+        opts = defaultSchemaOptions
+          { fieldLabelModifier = \case
+              "prekeyId" -> "id"
+              "prekeyKey" -> "key"
+          }
+
+instance ToSchema LastPrekey where
+    declareNamedSchema _ = declareNamedSchema (Proxy @Prekey)
+
+
+----------------------------------------------------------------------
+-- helpers
+
+camelToUnderscore :: String -> String
+camelToUnderscore = concatMap go . (ix 0 %~ toLower)
+  where go x = if isUpper x then "_" <> [toLower x] else [x]
